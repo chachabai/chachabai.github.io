740222fccbcd6c5e65e56055e21747e78c3376b0
chachabai <dna049@outlook.com>
2020-08-29 12:01:33 +0800

    Site updated: 2020-08-29 12:01:16

--- cf/contest/main.cpp
+++ cf/contest/main.cpp
@@ -1,72 +1,59 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long LL;
-const int N = 2e8+2;
-int p[N],pi[N];
-bool isp[N];
-int initprime(){
-    int cnt=1;p[1]=2;isp[2] = true;
-    for(int i=3;i<N;i+=2)   isp[i]=true;
-    for(int i=3;i<N;i+=2){
-        if(isp[i])  p[++cnt] = i;
-        for(int j = 2,t=(N-1)/i + 1;j <= cnt && p[j] < t; ++j){
-            isp[i * p[j]] = false;
-            if(i%p[j] == 0) break;
+#include <bits/stdc++.h>
+#define watch(x) std::cout << (#x) << " is " << (x) << std::endl
+#define print(x) std::cout << (x) << std::endl
+using LL = long long;
+struct Node {
+    int id, val, par, ch[2];
+    void init(int _id, int _val, int _par) {
+        id = _id, val = _val, par = _par, ch[0] = ch[1] = 0;
+    }
+};
+int cartesian_build(Node *tree, int n) {
+    for (int i = 1; i <= n; ++i) {
+        int k = i - 1;
+        while (tree[k].val > tree[i].val) k = tree[k].par;
+        tree[i].ch[0] = tree[k].ch[1];
+        tree[k].ch[1] = i;
+        tree[i].par = k;
+        tree[tree[i].ch[0]].par = i;
+    }
+    return tree[0].ch[1];
+}
+int main() {
+    //freopen("in","r",stdin);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int n;
+    while (std::cin >> n) {
+        if (n == 0) break;
+        Node tree[n + 1];
+        tree[0].init(0, 0, 0);
+        for (int i = 1, x; i <= n; ++i) {
+            std::cin >> x;
+            tree[i].init(i, x, 0);
         }
-    }
-    return cnt;
-}
-const int M = 7;
-const int PM = 2*3*5*7*11*13*17;
-int phi[PM+1][M+1],sz[M+1];
-void init(){
-    initprime();
-    pi[2] = 1;
-    for(int i=3;i<N;++i){
-        if(isp[i])  pi[i]=pi[i-1]+1;
-        else  pi[i]=pi[i-1];
-    }
-    sz[0]=1;
-    for(int i=0;i<=PM;++i)  phi[i][0]=i;
-    for(int i=1;i<=M;++i){
-        sz[i]=p[i]*sz[i-1];
-        for(int j=1;j<=PM;++j){
-            phi[j][i]=phi[j][i-1]-phi[j/p[i]][i-1];
-        }
-    }
-}
-LL primepi(LL x);
-LL primephi(LL x, int s){
-    if(s <= M)  return phi[x%sz[s]][s]+(x/sz[s])*phi[sz[s]][s];
-    if(x/p[s] <= p[s])  return primepi(x)-s+1;
-    if(x<N && x/p[s]/p[s] <= p[s]){
-        int s2x = pi[(int)(sqrt(x+0.2))];
-        LL ans = pi[x]-LL(s2x+s-2)*(s2x-s+1)/2;
-        for(int i=s+1;i<=s2x;++i){
-            ans+=pi[x/p[i]];
-        }
-        return ans;
-    }
-    return primephi(x,s-1)-primephi(x/p[s],s-1);
-}
-LL primepi(LL x){
-    if(x<N) return pi[x];
-    int ps2x = pi[int(sqrt(x+0.2))];
-    int ps3x = pi[int(cbrt(x+0.2))];
-    LL ans = primephi(x,ps3x) + LL(ps2x+ps3x-2)*(ps2x-ps3x+1)/2;
-    for(int i =ps3x+1,ed = ps2x;i<=ed;++i){
-        ans -= primepi(x/p[i]);
-    }
-    return ans;
-}
-
-int main(){
-    init();
-    LL n;
-    while(cin>>n){ // n=98765432109876 = 9.8*10^13 用时 38s, N 大点，耗时会小点
-        time_t now = time(0);
-        cout<<primepi(n)<<endl;
-        cout<<"Time used: "<<difftime(time(0),now)<<endl;
+        int root = cartesian_build(tree, n);
+		// 下面是求区间
+		int l[n + 1], r[n + 1];
+		std::function<void(int)> getinterval = [&](int x) {
+			if (x == 0) return;
+			if (tree[tree[x].par].ch[0] == x) {
+				r[x] = tree[x].par - (tree[x].val != tree[tree[x].par].val);
+				l[x] = l[tree[x].par];
+			} else {
+				l[x] = tree[x].par + (tree[x].val != tree[tree[x].par].val);
+				r[x] = r[tree[x].par];
+			}
+			getinterval(tree[x].ch[0]);
+			getinterval(tree[x].ch[1]);
+		};
+		l[root] = 1;
+		r[root] = n;
+		getinterval(tree[root].ch[0]);
+		getinterval(tree[root].ch[1]);
+		for (int i = 1; i <= n; ++i) {
+			std::cout << l[i] << " " << r[i] << std::endl;
+		}
     }
     return 0;
 }
