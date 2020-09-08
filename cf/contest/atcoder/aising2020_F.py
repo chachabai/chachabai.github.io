8eb51a428d3ef9155e26ee033884130a51849a79
chachabai <dna049@outlook.com>
2020-09-09 03:35:41 +0800

    Site updated: 2020-09-09 03:35:30

--- cf/contest/atcoder/aising2020_F.py
+++
@@ -1,9 +0,0 @@
-import math
-M = 1000000007
-T = int(input())
-for i in range(T):
-	n = int(input())
-	r = 0
-	for i in range((n-5)%2,12,2):
-		r+=math.comb(11,i)%M*math.comb((n-i+25)//2,15)%M;
-	print(r%M)
